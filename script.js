e223fb1a01209cd0244664fc838a55550d5e2489
minhduc <minhduc16102@gmail.com>
2023-06-08 21:43:47 +0700

    web: add colorbar, latest entries list, modify bindPopup

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 $(document).ready(function () {
-  // Tạo bản đồ và đặt tâm ở vị trí ban đầu
-  var map = L.map("map").setView([10.834652, 106.766828], 13);
+  var map = L.map("map").setView([0.0, 0.0], 13);
+  var markerLayer = L.layerGroup().addTo(map); // Create a layer group for markers
 
-  // Thêm Layer bản đồ từ OpenStreetMap
+  // Add OpenStreetMap tile layer
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 18,
   }).addTo(map);
 
-  // Lấy dữ liệu từ Adafruit IO
-  var aioFeedKey = "aio_ytot39or1nRzN5T2KMuSJrO8yCrR"; // Thay YOUR_FEED_KEY bằng khóa Feed của bạn
-  var aioUsername = "minhduco19"; // Thay YOUR_USERNAME bằng tên người dùng của bạn
-  var aioFeedName = "nb-iot-tracking.nb-iot"; // Thay YOUR_FEED_NAME bằng tên Feed của bạn
+  var aioFeedKey = "aio_ytot39or1nRzN5T2KMuSJrO8yCrR";
+  var aioUsername = "minhduco19";
+  var aioFeedName = "nb-iot-tracking.nb-iot";
+  var aioURL = `https://io.adafruit.com/api/v2/${aioUsername}/feeds/${aioFeedName}/data`;
 
-  var aioURL =
-    "https://io.adafruit.com/api/v2/" +
-    aioUsername +
-    "/feeds/" +
-    aioFeedName +
-    "/data";
+  var latestPoint;
+  var start = 0;
 
   function getDataAndUpdateMap() {
     $.ajax({
@@ -28,75 +24,83 @@ $(document).ready(function () {
         "X-AIO-Key": aioFeedKey,
       },
       success: function (data) {
-        // Xóa các đối tượng đánh dấu hiện tại trên bản đồ
-        map.eachLayer(function (layer) {
-          if (layer instanceof L.Marker) {
-            map.removeLayer(layer);
-          }
-        });
-        var values = {};
+        // Clear existing markers on the map
+        markerLayer.clearLayers();
+
         var latestData = data.slice(0, 10);
-        // Xử lý dữ liệu mới và hiển thị lên bản đồ
-        latestData.forEach(function (item) {
+        latestPoint = latestData[0]; // Get the latest data point
+        map.setView([latestPoint.lat, latestPoint.lon]);
+
+        if (start == 0) {
+          // Update the map view to the latest point
+          map.setView([latestPoint.lat, latestPoint.lon]);
+          start = 1;
+        }
+
+        latestData.forEach(function (item, index) {
           var lat = item.lat;
           var lon = item.lon;
           var value = item.value;
-          //console.log(value);
-          var st = value;
-          var colorStr;
-          // Tách chuỗi thành các cặp key-value
-          var pairs = st.split(",");
+
+          var pairs = value.split(",");
+          var values = {};
           pairs.forEach(function (pair) {
-            // Tách cặp key-value thành key và value
             var parts = pair.split(":");
             var key = parts[0];
             var val = parts[1];
-
-            // Lưu trữ giá trị vào đối tượng
             values[key] = val;
           });
-          console.log(values);
-          // Đánh giá chất lượng dựa trên giá trị dữ liệu
-          var markerColor = "#ff0000"; // Mặc định màu đỏ
+
           var temp = parseInt(values["rsrp"]);
-          console.log(temp);
+          var colorStr;
+
           if (temp >= -80 && temp < 0) {
             colorStr =
               "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png";
-            // markerColor = "#00ff00"; // Màu xanh lá cây cho chất lượng tốt
           } else if (temp >= -90 && temp < -80) {
             colorStr =
               "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-gold.png";
-            // markerColor = "#ffff00"; // Màu vàng cho chất lượng trung bình
           } else if (temp >= -90 && temp < -100) {
             colorStr =
               "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png";
-            // markerColor = "#ffa500"; // Màu cam cho chất lượng không tốt
           } else {
             colorStr =
               "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png";
-            // markerColor = "#ff0000"; // Màu đỏ cho chất lượng xấu
           }
-          // Tạo đối tượng đánh dấu và thiết lập màu sắc
+
           var marker = L.marker([lat, lon], {
             icon: L.icon({
               iconUrl: colorStr,
               iconSize: [25, 41],
               iconAnchor: [12, 41],
               popupAnchor: [1, -34],
-              // shadowUrl:
-              //   "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-              // shadowSize: [41, 41],
-              // shadowAnchor: [12, 41],
-              className: "custom-marker",
-              html:
-                '<div style="background-color:' +
-                markerColor +
-                '; width: 25px; height: 41px; border-radius: 50%;"></div>',
             }),
-          }).addTo(map);
-          marker.bindPopup("Giá trị: " + value);
+          });
+
+          var popupContent = "<table>";
+          pairs.forEach(function (pair) {
+            var parts = pair.split(":");
+            var key = parts[0];
+            var val = parts[1];
+            popupContent +=
+              "<tr><td><strong>" +
+              key +
+              "</strong></td><td>" +
+              val +
+              "</td></tr>";
+          });
+          popupContent +=
+            "<tr><td><strong>Quality</strong></td><td>" +
+            getSignalQuality(temp) +
+            "</td></tr>";
+          popupContent += "</table>";
+
+          marker.bindPopup(popupContent);
+          markerLayer.addLayer(marker); // Add the marker to the layer group
         });
+
+        // Generate and update the data table
+        generateTableRows(latestData);
       },
       error: function () {
         console.log("Lỗi khi lấy dữ liệu từ Adafruit IO");
@@ -104,11 +108,152 @@ $(document).ready(function () {
     });
   }
 
-  // Gọi hàm getDataAndUpdateMap ban đầu
+  function getSignalQuality(rsrp) {
+    if (rsrp >= -80 && rsrp < 0) {
+      return "Good";
+    } else if (rsrp >= -90 && rsrp < -80) {
+      return "Normal";
+    } else if (rsrp >= -100 && rsrp < -90) {
+      return "Bad";
+    } else {
+      return "Very Bad";
+    }
+  }
+
+  // Toggle the display of the data list container
+  function toggleList() {
+    $(".list-container").slideToggle();
+  }
+
+  // Function to generate table rows from the latest data
+  function generateTableRows(data) {
+    var table = $("#data-table");
+    var tableBody = table.find("tbody");
+    tableBody.empty(); // Clear existing table body content
+
+    // Create table rows
+    data.forEach(function (item, index) {
+      var id = index + 1; // Generate ID from index
+      var lat = item.lat;
+      var lon = item.lon;
+      var value = item.value;
+      var createdAt = new Date(item.created_at);
+
+      var pairs = value.split(",");
+      var values = {};
+      pairs.forEach(function (pair) {
+        var parts = pair.split(":");
+        var key = parts[0];
+        var val = parts[1];
+        values[key] = val;
+      });
+
+      var temp = parseInt(values["rsrp"]);
+      var colorStr;
+
+      if (temp >= -80 && temp < 0) {
+        colorStr = "green";
+      } else if (temp >= -90 && temp < -80) {
+        colorStr = "gold";
+      } else if (temp >= -90 && temp < -100) {
+        colorStr = "orange";
+      } else {
+        colorStr = "red";
+      }
+
+      var row = $("<tr></tr>")
+        .click(function () {
+          zoomToMarker(index);
+        })
+        .appendTo(tableBody);
+
+      $("<td></td>").css("color", colorStr).text(id).appendTo(row);
+      $("<td></td>").css("color", colorStr).text(lat).appendTo(row);
+      $("<td></td>").css("color", colorStr).text(lon).appendTo(row);
+      $("<td></td>").css("color", colorStr).text(values["pci"]).appendTo(row);
+      $("<td></td>").css("color", colorStr).text(values["rsrp"]).appendTo(row);
+      $("<td></td>").css("color", colorStr).text(values["rsrq"]).appendTo(row);
+      $("<td></td>").css("color", colorStr).text(values["sinr"]).appendTo(row);
+      $("<td></td>")
+        .css("color", colorStr)
+        .text(values["cellid"])
+        .appendTo(row);
+      $("<td></td>")
+        .css("color", colorStr)
+        .text(
+          createdAt.toLocaleString("en-GB", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            timeZone: "Asia/Bangkok",
+          })
+        )
+        .appendTo(row); // Convert createdAt to desired format
+    });
+  }
+
+  // Function to zoom and center the map to the marker associated with the clicked row
+  function zoomToMarker(index) {
+    var marker = markerLayer.getLayers()[index];
+    if (marker) {
+      map.setView(marker.getLatLng());
+      marker.openPopup();
+    }
+  }
+
+  // Get data and update map initially
   getDataAndUpdateMap();
 
-  // Cập nhật dữ liệu mỗi 5 giây
+  // Update data and map every 5 seconds
   setInterval(function () {
     getDataAndUpdateMap();
-  }, 5000);
+  }, 300000);
+
+  // Add title to the table
+  $("<caption></caption>")
+    .addClass("table-title")
+    .text("10 LATEST ENTRIES")
+    .prependTo("#data-table");
+
+  $(".table-title").css({
+    "font-size": "24px",
+    "font-weight": "bold",
+    padding: "5.5px",
+  });
+
+  // Bind the toggleList function to the stripes button click event
+  $(".stripes-button").click(function () {
+    toggleList();
+  });
+
+  // Create a custom control for the color bar legend
+  var legendControl = L.control({
+    position: "bottomleft",
+  });
+
+  legendControl.onAdd = function (map) {
+    var legendDiv = L.DomUtil.create("div", "legend");
+
+    legendDiv.innerHTML = `
+    <div class="color-bar">
+      <span class="color-bar-item" style="background-color: green;"></span>
+      <span class="color-bar-item" style="background-color: gold;"></span>
+      <span class="color-bar-item" style="background-color: orange;"></span>
+      <span class="color-bar-item" style="background-color: red;"></span>
+    </div>
+    <div class="legend-labels">
+      <span class="legend-label">Good</span>
+      <span class="legend-label">Normal</span>
+      <span class="legend-label">Bad</span>
+      <span class="legend-label">Very Bad</span>
+    </div>
+  `;
+
+    return legendDiv;
+  };
+
+  // Add the legend control to the map
+  legendControl.addTo(map);
 });
